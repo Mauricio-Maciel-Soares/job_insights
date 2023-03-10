7d938b6a6f3a87986c43b9d55b78ed957fc1eb18
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2023-03-10 18:46:28 -0300

    update commit

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,51 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {
+            "title": "Web developer",
+            "min_salary": "invalid",
+            "max_salary": "invalid",
+        },
+        {
+            "title": "Front end developer",
+            "min_salary": "1000",
+            "max_salary": " ",
+        },
+        {
+            "title": "Back end developer",
+            "min_salary": " ",
+            "max_salary": "3000",
+        },
+        {
+            "title": "Full stack end developer",
+            "min_salary": "4000",
+            "max_salary": "8000",
+        },
+    ]
+    criteria = "min_salary"
+    result = sort_by(jobs, criteria)
+    expected = [
+        {
+            "title": "Front end developer",
+            "min_salary": "1000",
+            "max_salary": " ",
+        },
+        {
+            "title": "Full stack end developer",
+            "min_salary": "4000",
+            "max_salary": "8000",
+        },
+        {
+            "title": "Back end developer",
+            "min_salary": " ",
+            "max_salary": "3000",
+        },
+        {
+            "title": "Web developer",
+            "min_salary": "invalid",
+            "max_salary": "invalid",
+        },
+    ]
+    assert result == expected
